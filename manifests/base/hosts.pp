8a84000fd92f5a473b0aa22eba797a6e6285d91c
Marco Tulio R Braga <git@mtulio.eng.br>
2016-01-12 00:12:28 -0200

    Add /etc/hosts manage class

---
+++ manifests/base/hosts.pp
@@ -0,0 +1,13 @@
+# == Class: linux::base::hosts
+#
+# This module manages the /etc/hosts file.
+#
+
+class linux::base::hosts {
+
+  host { $fqdn:
+    ip => $ipaddress,
+    host_aliases => [ $hostname ],
+  }
+
+}
